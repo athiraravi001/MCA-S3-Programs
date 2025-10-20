e47b2553e261251540ccd4e106679ece7611f62a
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-10-20 17:07:01 +0530

    Add files via upload

---
+++ Data_Science_Lab/Matrix Operations/null_array.py
@@ -0,0 +1,7 @@
+# Create a null NumPy array of size 5 and update the 3rd value to 10.
+
+import numpy as np
+null_array=np.zeros(5)
+print("Null array : ", null_array)
+null_array[2]=10
+print("Updated null array : ", null_array)
