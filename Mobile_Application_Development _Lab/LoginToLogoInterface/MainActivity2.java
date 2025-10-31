db79a99ed5e83a70ed8ba401217d0e5be89c026c
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-11-01 00:15:03 +0530

    Add files via upload

--- Mobile_Application_Development _Lab/LoginToLogoInterface/MainActivity2.java
+++ Mobile_Application_Development _Lab/LoginToLogoInterface/MainActivity2.java
@@ -23,12 +23,12 @@ public class MainActivity2 extends AppCompatActivity {
     }
 
     public void onClick1(View view) {
-        logoDetails.setText("ChatGPT is an advanced AI chatbot that understands questions and responds in a human-like way.");
-        //Toast.makeText(MainActivity2.this, "ChatGPT is an advanced AI chatbot created by OpenAI that uses a large language model to understand questions and respond in a human-like way.", Toast.LENGTH_LONG).show();
+        logoDetails.setText("Copilot is an AI-powered virtual assistant that helps improve productivity by giving smart suggestions, automating tasks, and analyzing information.");
+        //Toast.makeText(MainActivity2.this, "Copilot is an AI-powered virtual assistant that helps improve productivity by giving smart suggestions, automating tasks, and analyzing information.", Toast.LENGTH_LONG).show();
     }
 
     public void onClick2(View view) {
-        logoDetails.setText("Copilot is an AI-powered virtual assistant that helps improve productivity by giving smart suggestions, automating tasks, and analyzing information.");
-        //Toast.makeText(MainActivity2.this, "Copilot is an AI-powered virtual assistant that helps improve productivity by giving smart suggestions, automating tasks, and analyzing information.", Toast.LENGTH_LONG).show();
+        logoDetails.setText("ChatGPT is an advanced AI chatbot that understands questions and responds in a human-like way.");
+        //Toast.makeText(MainActivity2.this, "ChatGPT is an advanced AI chatbot created by OpenAI that uses a large language model to understand questions and respond in a human-like way.", Toast.LENGTH_LONG).show();
     }
 }
