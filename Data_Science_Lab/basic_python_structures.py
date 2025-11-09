8981e3e9d945f44fd731c3ba5af993897f7e79d9
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-11-09 21:49:59 +0530

    Add files via upload

--- Data_Science_Lab/basic_python_structures.py
+++ Data_Science_Lab/basic_python_structures.py
@@ -99,5 +99,5 @@ print("Integer keys dictionary : ", integer_keys_dict)
 import numpy as np
 x = np.array([1,2,3,4,5,6,7,8,9,10])
 print("Array : ",x)
-y=x.tolist()
+y = x.tolist()
 print("List : ",y)
