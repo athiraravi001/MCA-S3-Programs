4a49d54a3959c64f3548663d05398b41366a6c39
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-11-09 16:09:11 +0530

    Add files via upload

---
+++ Data_Science_Lab/Data Visualization/countplot.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+data = {
+    "Name": ["Asha", "Arun", "Binu", "Devi", "Anu", "Jithu"],
+    "Department": ["CSE", "ECE", "CSE", "EEE", "CSE", "ECE"]
+}
+df = pd.DataFrame(data)
+sns.countplot(x="Department", data=df, hue="Department")
+plt.xlabel('Department')
+plt.ylabel('Name')
+plt.show()
+# p = sns.countplot(x="Department", data=df, hue="Name")
+# p.set_xlabel('Department')
+# p.set_ylabel('Name')
