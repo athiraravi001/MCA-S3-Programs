7e060f0589a5ab7287c6b0b38ddffda833b69b14
Athira P R <153584706+athiraravi001@users.noreply.github.com>
2025-11-10 23:08:37 +0530

    Add files via upload

--- Data_Science_Lab/Data Handling/handling_missing_data.py
+++ Data_Science_Lab/Data Handling/handling_missing_data.py
@@ -18,3 +18,22 @@ data = {
 df = pd.DataFrame(data)
 df['Marks'] = df['Marks'].fillna(df['Marks'].mean())      # Fill missing values in 'Marks' column with the average
 print(df)
+
+# -----------------------------------------------------------------------------------------------------------------
+# Write a Python program to create a DataFrame with missing values and show how to remove rows and columns containing missing values using dropna().
+
+"""
+import pandas as pd
+import numpy as np
+df = pd.DataFrame({
+                   'A': [1, 2, np.nan, 4],
+                   'B': [np.nan, 2, 3, 4]
+                 })
+print("Original DataFrame:\n", df)
+
+df_rows = df.dropna()
+print("\nAfter Dropping Rows with Missing Values:\n", df_rows)
+
+df_cols = df.dropna(axis=1)
+print("\nAfter Dropping Columns with Missing Values:\n", df_cols)
+"""
